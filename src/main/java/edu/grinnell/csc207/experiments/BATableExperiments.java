03b3fb3583dde2dcd0917ac87244af84661ef5b4
gorrelln <gorrelln@grinnell.edu>
2024-12-03 20:48:02 -0600

    added name to files

--- src/main/java/edu/grinnell/csc207/experiments/BATableExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/BATableExperiments.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 /**
  * Some experiments with the Braille-Ascii Tables.
  *
- * @author Your Name Here
+ * @author Nicole Gorrell
  * @author Samuel A. Rebelsky
  */
 public class BATableExperiments {
--- src/main/java/edu/grinnell/csc207/experiments/BitTreeExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/BitTreeExperiments.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 /**
  * Some experiments with Bit Trees.
  *
- * @author Your Name Here
+ * @author Nicole Gorrell
  * @author Samuel A. Rebelsky
  */
 public class BitTreeExperiments {
